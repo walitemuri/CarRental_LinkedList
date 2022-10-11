02e8e47c021862f1913d79ffdb1cc230a42abffb
walitemuri <wmtemuri@gmail.com>
2022-10-11 01:01:38 -0400

    Test

--- q1.c
+++ q1.c
@@ -44,14 +44,19 @@ int main(void)
 
     do
     {
+        //Note to future -- Fix this 
         printf
-        ("(a) add a new car to the available-for-rent list,
-        (b) add a returned car to the available-for-rent list,
-        (c) add a returned car to the repair list,
-        (d) transfer a car from the repair list to the available-for-rent list,
-        (e) rent the first available car,
-        (f) print all the lists,
-        (g) quit.")
+        ("
+            1 - add a new car to the available-for-rent list,\n
+            add a re
+            asd
+            adturned car to the available-for-rent list,\n
+            (c) add a returned car to the repair list,\n
+            (d) transfer a car from the repair list to the available-for-rent list,\n
+            (e) rent the first available car,\n
+            (f) print all the lists,\n
+            (g) quit"
+        );
 
         switch (x)
         {
